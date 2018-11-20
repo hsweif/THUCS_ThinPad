23af441a718ef9f8eba4b580ff749134946262f2
ZhiyiChen <962584560@qq.com>
2018-11-20 08:34:27 +0800

    合并

--- ControlB.v
+++ ControlB.v
@@ -26,7 +26,7 @@ module ControlB(
 	always @(*) begin
 		if((instr[15:11] == 5'b11100 && (instr[1:0] == 2'b01 || instr[1:0] == 2'b11)) || (instr[15:11] == 5'b11101 && (instr[4:0] == 5'b01100 || instr[4:0] == 5'b01010 || instr[4:0] == 5'b01011 || instr[4:0] == 5'b01101 || instr[4:0] == 5'b00010)) || (instr[15:11] == 5'b01111 && instr[4:0] == 5'b00000))
 			controlB = 2'b00; //ry
-		else if((instr[15:11] = 5'b00110 && (instr[1:0] = 2'b00 || instr[1:0] = 2'b11)) || instr[15:11] = 5'b01000 || instr[15:11] = 5'b01001 || instr[15:11] = 5'b01011 || instr[15:8] = 8'b01100011 || instr[15:11] = 5'b01101 || instr[15:11] = 5'b10010 || instr[15:11] = 5'b10011 || instr[15:11] = 5'b11010 || instr[15:11] = 5'b11011)
+		else if((instr[15:11] == 5'b00110 && (instr[1:0] == 2'b00 || instr[1:0] == 2'b11)) || instr[15:11] == 5'b01000 || instr[15:11] == 5'b01001 || instr[15:11] == 5'b01011 || instr[15:8] == 8'b01100011 || instr[15:11] == 5'b01101 || instr[15:11] == 5'b10010 || instr[15:11] == 5'b10011 || instr[15:11] == 5'b11010 || instr[15:11] == 5'b11011)
 			controlB = 2'b01; //imm
 		else
 			controlB = 2'b10; // default value, 0
