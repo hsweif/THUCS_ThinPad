0085d39a006c6eb14fc6fae573b569cacd141cf0
ZhiyiChen <962584560@qq.com>
2018-11-25 21:45:01 +0800

    forward

--- Forwarding.v
+++ Forwarding.v
@@ -26,13 +26,59 @@ module Forwarding(
     input [3:0] R2,
 	 input [15:0] RData1,
     input [15:0] RData2,
-	 output [1:0] Forward,
-	 output [1:0] ForwardingA,
-	 output [1:0] ForwardingB
+	 input [1:0] MemControl,
+	 output reg [1:0] Forward,
+	 output reg [1:0] ForwardingA,
+	 output reg [1:0] ForwardingB
     );
 
-assign Forward = 2'b0;
-assign ForwardingA = 2'b0;
-assign ForwardingB = 2'b0;
+reg MemWrite = 0;
+
+always @ (*) begin
+	if (MemControl == 2'b10) begin
+		MemWrite = 1;
+	end
+	else
+		MemWrite = 0;
+	ForwardingA = 2'b00;
+	ForwardingB = 2'b00;
+	Forward = 2'b00;
+	if (MemWrite == 0) begin
+		if (WRegFW1 != 4'b1111) begin
+			if (WRegFW1 == R1) begin
+				ForwardingA = 2'b01;
+			end
+			if (WRegFW1 == R2) begin
+				ForwardingB = 2'b01;
+			end
+		end
+		if (WRegFW2 != 4'b1111) begin
+			if ((WRegFW2 == R1) && (WRegFW2 != WRegFW1)) begin
+				ForwardingA = 2'b10;
+			end
+			if ((WRegFW2 == R2) && (WRegFW2 != WRegFW1)) begin
+				ForwardingB = 2'b10;
+			end
+		end
+	end
+	else begin
+		if (WRegFW1 != 4'b1111) begin
+			if (WRegFW1 == R1) begin
+				ForwardingA = 2'b01;
+			end
+			if (WRegFW1 == R2) begin
+				Forward = 2'b01;
+			end
+		end
+		if (WRegFW2 != 4'b1111) begin
+			if ((WRegFW2 == R1) && (WRegFW2 != WRegFW1)) begin
+				ForwardingA = 2'b10;
+			end
+			if ((WRegFW2 == R2) && (WRegFW2 != WRegFW1)) begin
+				Forward = 2'b10;
+			end
+		end
+	end
+end
 
 endmodule
