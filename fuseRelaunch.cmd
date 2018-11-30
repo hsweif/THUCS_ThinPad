322cce1358566e4300756b15735b84506db1bfb8
jyx <jiyx16@mails.tsinghua.edu.cn>
2018-11-30 17:08:45 +0800

    尝试解决多个对流水线控制的冲突

--- fuseRelaunch.cmd
+++ fuseRelaunch.cmd
@@ -1,1 +1,1 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "D:/THUCS_ThinPad/testPipeLine_isim_beh.exe" -prj "D:/THUCS_ThinPad/testPipeLine_beh.prj" "testPipeLine" "work.glbl" 
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "C:/Users/jyx/Desktop/learn/Code/git/THUCS_ThinPad/hazard_isim_beh.exe" -prj "C:/Users/jyx/Desktop/learn/Code/git/THUCS_ThinPad/hazard_beh.prj" "hazard" "work.glbl" 
